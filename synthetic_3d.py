74b3b63fb4fe567ca15fe4da4dfcd190ffbda223
zzhang87 <thomas.zz.zhang@gmail.com>
2017-11-21 16:57:31 -0600

    debugging

--- synthetic_3d.py
+++ synthetic_3d.py
@@ -13,215 +13,256 @@ import pickle
 
 import filters
 
-def dist(x, y, x0, y0):
-	return np.sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0))
+class Burger(object):
+	def __init__(self, N_burger, Nx, Ny, Nz,
+					xmin, xmax, ymin, ymax, zmin, zmax,
+					gamma = 200, sigma = 0.01, nu = 1):
 
-def decompose(x, y, x0, y0, vel, R, sign):
-	dx = x - x0
-	dy = y - y0
-	u = sign * (- dy / R * vel) - 0.5 * 0.01 * dx
-	v = sign * (dx / R * vel) - 0.5 * 0.01 * dy
-	return u, v
+		self.xmin = xmin
+		self.xmax = xmax
 
-def velocity(d, gamma = 50, sigma = 0.01, nu = 1):
-	return gamma * (1 - np.exp(-sigma * d**2 / 4 / nu)) / (2 * np.pi * d)
+		self.ymin = ymin
+		self.ymax = ymax
 
-def Burger(X, Y, x0, y0, sign):
-	R = np.sqrt((X - x0) * (X - x0) + (Y - y0) * (Y - y0))
-	U = velocity(R)
-	vx, vy = decompose(X, Y, x0, y0, U, R, sign)
+		self.zmin = zmin
+		self.zmax = zmax
 
-	return vx, vy
+		x, dx = np.linspace(xmin, xmax, num = Nx, endpoint = True, retstep = True)
+		y, dy = np.linspace(ymin, ymax, num = Ny, endpoint = True, retstep = True)
+		z, dz = np.linspace(zmin, zmax, num = Nz, endpoint = True, retstep = True)
 
-def Vel(x, y, z, centroids, gamma = 50, sigma = 0.01, nu = 1):
-	ux = 0
-	uy = 0
+		self.X, self.Y, self.Z = np.meshgrid(x, y, z)
 
-	for i in range(len(centroids)):
-		x0 = centroids[i][0]
-		y0 = centroids[i][1]
-		sign = centroids[i][2]
+		self.centroids = []
 
-		new_vx, new_vy = Burger(x, y, x0, y0, sign)
+		for _ in range(N_burger):
+			x0 = random.uniform(xmin, xmax)
+			y0 = random.uniform(ymin, ymax)
+			sign = random.choice([-1, 1])
 
-		ux += new_vx
-		uy += new_vy
+			self.centroids.append((x0, y0, sign))
 
-	uz = sigma * z
+		self.gamma = gamma
+		self.sigma = sigma
+		self.nu = nu
 
-	return ux, uy, uz
+	def one_vortex(self, x0, y0, sign, x = None, y = None):
+		if x is None:
+			x = self.X
 
+		if y is None:
+			y = self.Y
 
-def Grad(x, y, centroids, axis, gamma = 50, sigma = 0.01, nu = 1):
-	du_dx = 0
-	du_dy = 0
+		R = np.sqrt((x - x0)**2 + (y - y0)**2)
+		U = self.gamma * (1 - np.exp(-self.sigma * R**2 / 4 / self.nu)) / (2 * np.pi * R)
 
-	for i in range(len(centroids)):
-		x0 = centroids[i][0]
-		y0 = centroids[i][1]
-		sign = centroids[i][2]
+		dx = x - x0
+		dy = y - y0
 
-		f = (x - x0)**2 + (y - y0)**2
+		u = sign * (- dy / R * U) - 0.5 * self.sigma * dx
+		v = sign * (dx / R * U) - 0.5 * self.sigma * dy
 
-		exp = np.exp(-sigma * f / (4 * nu))
+		return u, v
 
-		if axis == 'X':
-			du_dx += sign * (x - x0) * (y - y0) * gamma * ((1 - exp) / (np.pi * f**2) - 
-						sigma * exp / (4 * nu) / (np.pi * f) ) - sigma / 2
-
-			du_dy += sign * gamma * (- (1 - exp) / (2 * np.pi * f) +
-						(y - y0)**2 * ((1 - exp) / (np.pi * f**2) - 
-						sigma * exp / (4 * nu) / (np.pi * f) ) )
-
-			du_dz = 0
-
-		elif axis == 'Y':
-			du_dx += sign * gamma * ((1 - exp) / (2 * np.pi * f) -
-						(x - x0)**2 * ((1 - exp) / (np.pi * f**2) - 
-						sigma * exp / (4 * nu) / (np.pi * f) ) )
-
-			du_dy += sign * (x - x0) * (y - y0) * gamma * (-(1 - exp) / (np.pi * f**2) + 
-						sigma * exp / (4 * nu) / (np.pi * f) ) - sigma / 2
-
-			du_dz = 0
-
-		elif axis == 'Z':
-			du_dx = 0
-			du_dy = 0
-			du_dz = sigma
-
-		else:
-			raise(Error("Invaid axis"))
-
-	return du_dx, du_dy, du_dz
-
-def checkBoundary(streamline, t, xmin = -50, xmax = 50, ymin = -50, ymax = 50,
-					zmin = 0, zmax = 100):
-	for i in range(len(t)):
-		x = streamline[i][0]
-		y = streamline[i][1]
-		z = streamline[i][2]
-
-		if x > xmin and x < xmax and y > ymin and y < ymax and z > zmin and z < zmax:
-			continue
-
-		else:
-			break
-
-	return streamline[:i,:], t[:i]
+	def flow_field(self):
+		self.vx = np.zeros(self.X.shape)
+		self.vy = np.zeros(self.Y.shape)
+		self.vz = np.zeros(self.Z.shape)
 
 
-def flow_field(N, Nx, Ny, Nz, xmin, xmax, ymin, ymax, zmin, zmax):
-	x, dx = np.linspace(xmin, xmax, num = Nx, endpoint = True, retstep = True)
-	y, dy = np.linspace(ymin, ymax, num = Ny, endpoint = True, retstep = True)
-	z, dz = np.linspace(zmin, zmax, num = Nz, endpoint = True, retstep = True)
+		for c in self.centroids:
 
-	X, Y, Z = np.meshgrid(x, y, z)
-
-	vx = np.zeros(X.shape)
-	vy = np.zeros(Y.shape)
-	vz = np.zeros(Z.shape)
-
-	centroids = []
-	for _ in range(N):
-		x0 = random.uniform(-50, 50)
-		y0 = random.uniform(-50, 50)
-		sign = random.choice([-1, 1])
-
-		new_vx, new_vy = Burger(X, Y, x0, y0, sign)
+			new_vx, new_vy = self.one_vortex(c[0], c[1], c[2])
 		
-		vx += new_vx
-		vy += new_vy
+			self.vx += new_vx
+			self.vy += new_vy
 
-		centroids.append((x0, y0, sign))
+		self.vz = self.sigma * self.Z
 
-	vz = 0.01 * Z
+	def get_Vel(self, x, y, z):
+		ux = 0
+		uy = 0
 
-	return X, Y, Z, vx, vy, vz, centroids
+		for c in self.centroids:
+			x0 = c[0]
+			y0 = c[1]
+			sign = c[2]
 
-def trajectory(N, X, Y, Z, vx, vy, vz, centroids, dt, t1):
-	grid_arr = np.array(zip(X.flatten(), Y.flatten(), Z.flatten()))
-	dfun = lambda p,t: [interp.griddata(grid_arr, vx.flatten(), np.array([p]))[0], 
-						interp.griddata(grid_arr, vy.flatten(), np.array([p]))[0],
-						interp.griddata(grid_arr, vz.flatten(), np.array([p]))[0]]
+			new_vx, new_vy = self.one_vortex(x0, y0, sign, x, y)
+
+			ux += new_vx
+			uy += new_vy
+
+		uz = self.sigma * z
+
+		return ux, uy, uz
+
+	def get_Grad(self, x, y, axis):
+		du_dx = 0
+		du_dy = 0
+
+		for c in self.centroids:
+			x0 = c[0]
+			y0 = c[1]
+			sign = c[2]
+
+			f = (x - x0)**2 + (y - y0)**2
+
+			exp = np.exp(-self.sigma * f / (4 * self.nu))
+
+			if axis == 'X':
+				du_dx += sign * (x - x0) * (y - y0) * self.gamma * ((1 - exp) / (np.pi * f**2) - 
+							self.sigma * exp / (4 * self.nu) / (np.pi * f) ) - self.sigma / 2
+
+				du_dy += sign * self.gamma * (- (1 - exp) / (2 * np.pi * f) +
+							(y - y0)**2 * ((1 - exp) / (np.pi * f**2) - 
+							self.sigma * exp / (4 * self.nu) / (np.pi * f) ) )
+
+				du_dz = 0
+
+			elif axis == 'Y':
+				du_dx += sign * self.gamma * ((1 - exp) / (2 * np.pi * f) -
+							(x - x0)**2 * ((1 - exp) / (np.pi * f**2) - 
+							self.sigma * exp / (4 * self.nu) / (np.pi * f) ) )
+
+				du_dy += sign * (x - x0) * (y - y0) * self.gamma * (-(1 - exp) / (np.pi * f**2) + 
+							self.sigma * exp / (4 * self.nu) / (np.pi * f) ) - self.sigma / 2
+
+				du_dz = 0
+
+			elif axis == 'Z':
+				du_dx = 0
+				du_dy = 0
+				du_dz = self.sigma
+
+			else:
+				raise(Error("Invaid axis"))
+
+		return du_dx, du_dy, du_dz
+
+	def checkBoundary(self, streamline, t):
+		for i in range(len(t)):
+			x = streamline[i][0]
+			y = streamline[i][1]
+			z = streamline[i][2]
+
+			if x > self.xmin and x < self.xmax \
+				and y > self.ymin and y < self.ymax \
+				and z > self.zmin and z < self.zmax:
+
+				continue
+
+			else:
+				break
+
+		return streamline[:i,:], t[:i]
+
+	def trajectory(self, N_traj, dt, t_end):
+		grid_arr = np.array(zip(self.X.flatten(), self.Y.flatten(), self.Z.flatten()))
+		dfun = lambda p,t: [interp.griddata(grid_arr, self.vx.flatten(), np.array([p]))[0], 
+							interp.griddata(grid_arr, self.vy.flatten(), np.array([p]))[0],
+							interp.griddata(grid_arr, self.vz.flatten(), np.array([p]))[0]]
 
 
-	t = np.arange(0,t1+dt,dt)
-	streamline = []
-	T = []
-	U = []
-	A = []
-	# A_cs = []
+		t = np.arange(0,t_end+dt,dt)
+		self.streamline = []
+		self.T = []
+		self.U = []
+		self.A = []
 
-	# css = []
+		for i in range(N_traj):
+			p0 = (random.uniform(self.xmin, self.xmax), 
+					random.uniform(self.ymin, self.ymax), 
+					random.uniform(self.zmin, self.zmax))
 
-	for i in range(N):
-		p0 = (random.uniform(-50, 50), random.uniform(-50, 50), random.uniform(0, 100))
-		streamline.append(integrate.odeint(dfun,p0,t))
-		streamline[i], temp = checkBoundary(streamline[i], t)
+			streamline = integrate.odeint(dfun, p0, t)
+			streamline, temp = self.checkBoundary(streamline, t)
 
-		T.append(temp)
-		U.append(np.empty((len(T[i]), 4)))
-		A.append(np.empty((len(T[i]), 4)))
+			self.streamline.append(streamline)
+			self.T.append(temp)
+			self.U.append(np.empty((len(self.T[i]), 4)))
+			self.A.append(np.empty((len(self.T[i]), 4)))
 
-		for j in range(len(T[i])):
-			x = streamline[i][j,0]
-			y = streamline[i][j,1]
-			z = streamline[i][j,2]
+			for j in range(len(self.T[i])):
+				x = self.streamline[i][j,0]
+				y = self.streamline[i][j,1]
+				z = self.streamline[i][j,2]
 
-			ux, uy, uz = Vel(x, y, z, centroids)
+				ux, uy, uz = self.get_Vel(x, y, z)
 
-			U[i][j,0] = ux
-			U[i][j,1] = uy
-			U[i][j,2] = uz
+				self.U[i][j,0] = ux
+				self.U[i][j,1] = uy
+				self.U[i][j,2] = uz
 
-			du_dx, du_dy, du_dz = Grad(x, y, centroids, 'X')
+				du_dx, du_dy, du_dz = self.get_Grad(x, y, 'X')
 
-			A[i][j,0] = ux * du_dx + uy * du_dy + uz * du_dz
+				self.A[i][j,0] = ux * du_dx + uy * du_dy + uz * du_dz
 
-			du_dx, du_dy, du_dz = Grad(x, y, centroids, 'Y')
+				du_dx, du_dy, du_dz = self.get_Grad(x, y, 'Y')
 
-			A[i][j,1] = ux * du_dx + uy * du_dy + uz * du_dz
+				self.A[i][j,1] = ux * du_dx + uy * du_dy + uz * du_dz
 
-			du_dx, du_dy, du_dz = Grad(x, y, centroids, 'Z')
+				du_dx, du_dy, du_dz = self.get_Grad(x, y, 'Z')
 
-			A[i][j,2] = ux * du_dx + uy * du_dy + uz * du_dz
+				self.A[i][j,2] = ux * du_dx + uy * du_dy + uz * du_dz
 
-		U[i][:,-1] = np.sqrt(U[i][:,0]**2 + U[i][:,1]**2 + U[i][:,2]**2)
-		A[i][:,-1] = np.sqrt(A[i][:,0]**2 + A[i][:,1]**2 + A[i][:,2]**2)
+			self.U[i][:,-1] = np.sqrt(self.U[i][:,0]**2 + self.U[i][:,1]**2 + self.U[i][:,2]**2)
+			self.A[i][:,-1] = np.sqrt(self.A[i][:,0]**2 + self.A[i][:,1]**2 + self.A[i][:,2]**2)
+
+		# return self.streamline, self.U, self.A, self.T
 
 
-		# csx = interp.CubicSpline(T[i], streamline[i][:,0])
-		# csy = interp.CubicSpline(T[i], streamline[i][:,1])
-		# css.append((csx, csy))
+	def distort(self, skip, delta):
+		streamline = []
+		U = []
+		A = []
+		T = []
 
-		# ax = csx(T[i], 2)
-		# ay = csy(T[i], 2)
-		# A_cs.append(np.sqrt(ax**2 + ay**2))
+		for i in range(len(self.T)):
+			streamline.append(self.streamline[i][::skip])
+			streamline[i] += delta * np_random.rand(streamline[i].shape[0],
+														streamline[i].shape[1])
+			U.append(self.U[i][::skip])
+			A.append(self.A[i][::skip])
+			T.append(self.T[i][::skip])
 
-	return streamline, U, A, T
+		return streamline, U, A, T
 
-def distort(streamline, U, A, T, skip, delta):
-	new_streamline = []
-	new_U = []
-	new_A = []
-	new_T = []
 
-	for i in range(len(T)):
-		new_streamline.append(streamline[i][::skip])
-		new_streamline[i] += delta * np_random.rand(new_streamline[i].shape[0],
-													new_streamline[i].shape[1])
-		new_U.append(U[i][::skip])
-		new_A.append(A[i][::skip])
-		new_T.append(T[i][::skip])
+	def make_plot(self, U_hat, A_hat, T_hat):
+		plt.figure()
+		for i in range(len(T_hat)):
+			for n in U_hat.keys():
+				plt.plot(T_hat[i], U_hat[n][i][:,-1], '*', label = '{}_{}'.format(n, i))
+			plt.plot(self.T[i], self.U[i][:,-1], label = 'GT {}'.format(i))
+		plt.legend()
+		plt.grid()
 
-	return new_streamline, new_U, new_A, new_T
+		plt.figure()
+		for i in range(len(T_hat)):
+			for n in A_hat.keys():
+				plt.plot(T_hat[i], A_hat[n][i][:,-1], '*', label = '{}_{}'.format(n, i))
+			plt.plot(self.T[i], self.A[i][:,-1], label = 'GT {}'.format(i))
+		plt.legend()
+		plt.grid()
+
+
+		fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		for i in range(len(self.T)):
+			ax.plot(self.streamline[i][:,0], self.streamline[i][:,1], self.streamline[i][:,2],
+					label = 'Traj {}'.format(i))
+		plt.legend()
+
+		fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		ax.quiver(self.X, self.Y, self.Z, self.vx, self.vy, self.vz, length = 10.0)
 
 def Calc_error(U, U_hat, A, A_hat, T, l):
 	err_U = {}
 	err_A = {}
 
-	plt.figure()
+	# plt.figure()
 
 	for n in U_hat.keys():
 		err_U[n] = []
@@ -237,9 +278,9 @@ def Calc_error(U, U_hat, A, A_hat, T, l):
 			err_U[n].append( np.mean(np.abs(U[i][l:-l,-1] - U_hat[n][i][l:-l,-1]) / U[i][l:-l,-1]) )
 			err_A[n].append( np.mean(np.abs(A[i][l:-l,-1] - A_hat[n][i][l:-l,-1]) / A[i][l:-l,-1]) )
 
-			plt.plot(T[i][l:-l], U_hat[n][i][l:-l,-1], label = "{}_{}".format(n, i))
+			# plt.plot(T[i][l:-l], U_hat[n][i][l:-l,-1], label = "{}_{}".format(n, i))
 
-			plt.plot(T[i][l:-l], U[i][l:-l,-1], label = "GT_{}".format(i))
+			# plt.plot(T[i][l:-l], U[i][l:-l,-1], label = "GT_{}".format(i))
 
 
 		err_U[n] = np.mean(np.array(err_U[n]))
@@ -249,7 +290,7 @@ def Calc_error(U, U_hat, A, A_hat, T, l):
 		print ("Velocity {}: {}".format(n, err_U[n]))
 		print ("Acceleration {}: {}".format(n, err_A[n]))
 
-	plt.legend()
+	# plt.legend()
 
 	return err_U, err_A
 
@@ -260,39 +301,6 @@ def filter(names, streamline, T, l, dt):
 
 	return f.process()
 
-def make_plot(X, Y, Z, vx, vy, vz, streamline, U, U_hat, A, A_hat, T):
-	plt.figure(1)
-	for i in range(len(T)):
-		for n in U_hat.keys():
-			plt.plot(T[i], U_hat[n][i][:,-1], '*', label = '{}_{}'.format(n, i))
-		plt.plot(T[i], U[i][:,-1], label = 'GT {}'.format(i))
-		# plt.plot(T[i], U[i], label = 'U {}'.format(i))
-	plt.legend()
-	plt.grid()
-
-	plt.figure(2)
-	for i in range(len(T)):
-		for n in A_hat.keys():
-			plt.plot(T[i], A_hat[n][i][:,-1], '*', label = '{}_{}'.format(n, i))
-		plt.plot(T[i], A[i][:,-1], label = 'GT {}'.format(i))
-		# plt.plot(T[i], U[i], label = 'U {}'.format(i))
-	plt.legend()
-	plt.grid()
-
-
-	fig = plt.figure(3)
-	# plt.streamplot(X, Y, vx, vy)
-	ax = fig.gca(projection='3d')
-	for i in range(len(T)):
-		ax.plot(streamline[i][:,0], streamline[i][:,1], streamline[i][:,2],
-				label = 'Traj {}'.format(i))
-	plt.legend()
-
-	fig = plt.figure(4)
-	ax = fig.gca(projection='3d')
-	ax.quiver(X, Y, Z, vx, vy, vz, length = 10.0)
-	plt.show()
-
 def main():
 	xmin, xmax = -50, 50
 	ymin, ymax = -50, 50
@@ -301,16 +309,16 @@ def main():
 	Nx = Ny = Nz = 11
 
 	N_burger = 1
-	X, Y, Z, vx, vy, vz, centroids = flow_field(N_burger, Nx, Ny, Nz, 
-											xmin, xmax, ymin, ymax, zmin, zmax)
+	burger = Burger(N_burger, Nx, Ny, Nz, xmin, xmax, ymin, ymax, zmin, zmax, gamma = 100)
+	burger.flow_field()
 
 
 	dt = 0.1
 	t_end = 10
 	N_traj = 5
-	streamline, U, A, T = trajectory(N_traj, X, Y, Z, vx, vy, vz, centroids, dt, t_end)
+	burger.trajectory(N_traj, dt, t_end)
 
-	skip = np.linspace(1, 10, 5).astype(int)
+	skip = np.linspace(1, 10, 1).astype(int)
 	delta = np.linspace(1e-3, 1e-1, 1) 
 	N_sample = np.linspace(3, 5, 1)
 
@@ -332,15 +340,14 @@ def main():
 		for j in range(len(delta)):
 			for k in range(len(N_sample)):
 
-				streamline_new, U_new, A_new, T_new = distort(streamline, U, A, T,
-																skip[i], delta[j])
+				streamline, U, A, T = burger.distort(skip[i], delta[j])
 
 				dt_ = dt * skip[i]
 				l = int(N_sample[k] / dt_ + 0.5)
 
-				U_hat, A_hat = filter(names, streamline_new, T_new, l, dt_)
+				U_hat, A_hat = filter(names, streamline, T, l, dt_)
 
-				err_U_, err_A_ = Calc_error(U_new, U_hat, A_new, A_hat, T_new, l)
+				err_U_, err_A_ = Calc_error(U, U_hat, A, A_hat, T, l)
 
 				for n in names:
 					err_U[n][i,j,k] = err_U_[n]
@@ -352,7 +359,8 @@ def main():
 
 	with open("err_A", 'w') as f:
 		pickle.dump(err_A, f)
-	# make_plot(X, Y, Z, vx, vy, vz, streamline, U, U_hat, A, A_hat, T)
+
+	burger.make_plot(U_hat, A_hat, T)
 	plt.show()
 
 if __name__ == "__main__":
